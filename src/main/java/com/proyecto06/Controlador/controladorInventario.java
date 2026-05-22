a90c8b61ab65aad67f72e084f8ec3ce23952fcd9
Mya000 <cornejomya35@gmail.com>
2026-05-21 20:33:23 -0500

    Add controladorInventario for inventory management

---
+++ src/main/java/com/proyecto06/Controlador/controladorInventario.java
@@ -0,0 +1,14 @@
+package com.proyecto06;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class controladorInventario {
+
+    @GetMapping("/inventario")
+    public String inventario() {
+        return "html/inventario";
+    }
+
+}
