0455f43e0e5ca586b5db786af18cdd91cb29c0e3
LAB-USR-LSUR <LAB-USR-LSUR@grupoutp.net>
2026-05-27 14:28:23 -0500

    Resolver conflictos

--- src/main/java/com/proyecto06/Controlador/controladorInventario.java
+++ src/main/java/com/proyecto06/Controlador/controladorInventario.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 package com.proyecto06.Controlador;
-=======
-package com.proyecto06;
->>>>>>> 44aa0f066d2b0448dcc41f2fce695f99c1b446ca
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
